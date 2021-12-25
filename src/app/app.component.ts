d633ca724849b16ea797986357d73a70e323bad7
Ho3einzargar <Ho3einzargar@yahoo.com>
2021-12-25 16:01:01 +0330

    add file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormGroup,FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Forms-Task';
+    profileForm = new FormGroup({
+      username: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required)
+    }
+    )
+
+    onSubmit(){
+    console.log(this.profileForm.value);
+    
+  }
 }
